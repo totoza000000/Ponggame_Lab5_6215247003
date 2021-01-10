c5dc689c0b33d3b6123b5312582bf79c6279f52b
Thanathip <lekkun40@gmail.com>
2021-01-10 20:46:25 +0700

    1/10/2021

--- app/src/main/java/com/example/lab05_6215247003_ponggame/Bat.java
+++ app/src/main/java/com/example/lab05_6215247003_ponggame/Bat.java
@@ -3,55 +3,57 @@ package com.example.lab05_6215247003_ponggame;
 import android.graphics.RectF;
 
 public class Bat {
+
     final int STOPPED = 0;
     final int LEFT = 1;
     final int RIGHT = 2;
-
-
     private final RectF mRect;
     private final float mLength;
     private final float mBatSpeed;
     private final int mScreenX;
-    private float mXCooed;
+    private float mXCoord;
     private int mBatMoving = STOPPED;
 
     Bat(int sx, int sy) {
         mScreenX = sx;
         mLength = mScreenX / 8;
         float height = sy / 40;
-        mXCooed = mScreenX / 2;
-        float mYCooed = sy - height;
-        mRect = new RectF(mXCooed, mYCooed,
-                mXCooed + mLength,
-                mYCooed + height);
+        mXCoord = mScreenX / 2;
+        float mYCoord = sy - height;
+        mRect = new RectF(mXCoord, mYCoord,
+                mXCoord + mLength,
+                mYCoord + height);
         mBatSpeed = mScreenX;
     }
+
     RectF getRect() {
         return mRect;
 
     }
+
     void setMovementState(int state) {
 
         mBatMoving = state;
 
     }
-    void update(long fps) {
 
+
+    void update(long fps) {
+// Move the bat based on the mBatMoving variable and the speed of the previous frame
         if (mBatMoving == LEFT) {
-            mXCooed = mXCooed - mBatSpeed / fps;
+            mXCoord = mXCoord - mBatSpeed / fps;
         }
         if (mBatMoving == RIGHT) {
-            mXCooed = mXCooed + mBatSpeed / fps;
+            mXCoord = mXCoord + mBatSpeed / fps;
         }
-
-        if (mXCooed < 0) {
-            mXCooed = 0;
-        } else if (mXCooed + mLength > mScreenX) {
-            mXCooed = mScreenX - mLength;
+// Stop the bat going off the screen
+        if (mXCoord < 0) {
+            mXCoord = 0;
+        } else if (mXCoord + mLength > mScreenX) {
+            mXCoord = mScreenX - mLength;
         }
-
-        mRect.left = mXCooed;
-        mRect.right = mXCooed + mLength;
+// Update mRect based on the results from the previous code in update
+        mRect.left = mXCoord;
+        mRect.right = mXCoord + mLength;
     }
-
 }
